a38a479fd799e6c6e65b45d67eaf61b1febe80aa
Nadezhda Samoilova <nadesa,ojlova@yandex.ru>
2023-05-04 21:37:01 +0300

    My first commit

---
+++ create_user_test.py
@@ -0,0 +1,78 @@
+import sender_stand_request
+import data
+# эта функция меняет значения в параметре firstName
+def get_user_body(first_name):
+    # копирование словаря с телом запроса из файла data, чтобы не потерять данные в исходном словаре
+    current_body = data.user_body.copy()
+    # изменение значения в поле firstName
+    current_body["firstName"] = first_name
+    # возвращается новый словарь с нужным значением firstName
+    return current_body
+
+
+
+# Функция для позитивной проверки
+def positive_assert(first_name):
+    # В переменную user_body сохраняется обновленное тело запроса
+    user_body = get_user_body(first_name)
+    # В переменную user_response сохраняется результат запроса на создание пользователя:
+    user_response = sender_stand_request.post_new_user(user_body)
+
+    # Проверяется, что код ответа равен 201
+    assert user_response.status_code == 201
+    # Проверяется, что в ответе есть поле authToken, и оно не пустое
+    assert user_response.json()["authToken"] != ""
+
+    # В переменную users_table_response сохраняется результат запроса на получение данных из таблицы user_model
+    users_table_response = sender_stand_request.get_users_table()
+
+    # Строка, которая должна быть в ответе
+    str_user = user_body["firstName"] + "," + user_body["phone"] + "," \
+               + user_body["address"] + ",,," + user_response.json()["authToken"]
+
+    # Проверка, что такой пользователь есть, и он единственный
+    assert users_table_response.text.count(str_user) == 1
+
+# Тест 1. Успешное создание пользователя
+# Параметр fisrtName состоит из 2 символов
+def negative_assert_symbol(first_name):
+    # В переменную user_body сохраняется обновлённое тело запроса
+    user_body = get_user_body(first_name)
+
+    # В переменную response сохраняется результат запроса
+    response = sender_stand_request.post_new_user(user_body)
+
+    # Проверка, что код ответа равен 400
+    assert response.status_code == 400
+
+    # Проверка, что в теле ответа атрибут "code" равен 400
+    assert response.json()["code"] == 400
+    # Проверка текста в теле ответа в атрибуте "message"
+    assert response.json()["message"] == "Имя пользователя введено некорректно. " \
+                                         "Имя может содержать только русские или латинские буквы, " \
+                                         "длина должна быть не менее 2 и не более 15 символов"
+
+
+def negative_assert_no_firstname(user_body):
+    response = sender_stand_request.post_new_user(user_body)
+    assert response.status_code == 400
+    assert response.json()["code"] == 400
+    assert response.json()["message"] == "Не все необходимые параметры были переданы"
+
+# Тест 10. Ошибка
+# В запросе нет параметра firstName
+
+
+
+# Тест 11. Ошибка
+# Параметр fisrtName состоит из пустой строки
+# Тест 1. Успешное создание пользователя
+    # Параметр fisrtName состоит из 15 символов
+def test_create_user_15_letter_in_first_name_get_success_response():
+        positive_assert("Ааааааааааааааа")
+
+
+
+
+
+
